9b2d4dfa0e9025dac73aa80806cefdc1f49b9404
Ryan Rice <38504074+RiceR1999@users.noreply.github.com>
2018-12-10 18:20:32 -0500

    Update Customer.java
    customer final

--- Customer.java
+++ Customer.java
@@ -1,23 +1,15 @@
 /*
-
  * Purpose: Data Structure and Algorithms Final Project
-
  * Status: Complete and thoroughly tested
-
  * Last update: 11/21/18
-
  * Comment: Customer class which manages customer name and shopping cart
-
  * @author: Ryan Rice 
-
  * @version: 2018.11.21
-
  */
-import java.util.ArrayList;
 public class Customer {
 	
 	private String name;
-	private ArrayList<String> cart;
+	private int cart;
 	
 	/**
 	 * Constructor for customer class
@@ -26,16 +18,7 @@ public class Customer {
 	public Customer(String name)
 	{
 		this.name = name;
-		this.cart = new ArrayList<>();
-	}
-	
-		/**
-	 * Accessor method that returns the size of the customer's cart
-	 * @return cart.size()
-	 */
-	public int getCartSize()
-	{
-		return cart.size();
+		this.cart = 0;
 	}
 	
 	/**
@@ -47,54 +30,26 @@ public class Customer {
 		return name;
 	}
 	
+	public int sizeOfCart()
+	{
+		return cart;
+	}
 	/**
 	 * Since our driver will not have the ability to access the array list of the customer sub class 
 	 * we must create methods to do so
 	 * @param item
 	 */
-	public void addToCart(String item)
+	public void addToCart()
 	{
-		cart.add(item);
+		cart++;
 	}
 	
 	/**
-	 * Finds and removes a given shopping cart item
-	 * @param item
+	 * Removes the first item in the list
 	 */
-	public void removeFromCart(String item)
+	public void removeFromCart()
 	{
-		boolean found = false;
-		for(int x = 0; x < cart.size(); x++)
-		{
-			if(cart.get(x).equals(item))
-			{
-				found = true;
-				cart.remove(x);
-				System.out.println("Item successfully removed!");
-			}
-		}
-		if(found)
-		{
-			System.out.println("Item not found!");
-		}
+		cart--;
 	}
-	
-	/**
-	 * Finds and returns a given item
-	 * @param item
-	 */
-	public String getFromCart(String item)
-	{
-		String theString = "";
-		boolean found = false;
-		for(int x = 0; x < cart.size(); x++)
-		{
-			if(cart.get(x).equals(item))
-			{
-				found = true;
-				theString = cart.get(x);
-			}
-		}
-		return theString;
-	}
-}
+
+
